3c54ace67261c4a47b450d12d943cc6b158d5600
yassineedev <yassine.boussif18@gmail.com>
2026-02-07 21:45:02 +0100

    the drawer is working with curses

--- maze_drawer.py
+++ maze_drawer.py
@@ -1,5 +1,5 @@
-from mazegen import MazeGenerator
-import time
+from mazegen import MazeGenerator, Cell
+import curses
 
 maze = MazeGenerator.generate
 
@@ -7,18 +7,52 @@ maze = MazeGenerator.generate
 class MazeDrawer:
     def __init__(self, maze, stdscr):
         self.maze = maze
-        self.stdscr = stdscr
+        self.stdscr: curses.window = stdscr
 
     def draw(self, cell_size=2):
         self.stdscr.clear()
-        height, width = self.stdscr.getmaxyx()
-        cell_size = 3
-        yy, x_ = 20, 30
-        chars = x_ * cell_size + 1
-        self.stdscr.addstr(0, 0, "█" * chars)
-        for y in range(height):
-            line = "█"
-            
+        height = len(self.maze.grid)
+        width = len(self.maze.grid[0]) if height > 0 else 0
+        curses.start_color()
+        curses.use_default_colors()
+
+        try:
+            # Initialize colors
+            curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+            curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+            curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+            for y, row in enumerate(self.maze.grid):
+                for x, cell in enumerate(row):
+                    cy = y * 2
+                    cx = x * 3
+                    cell: Cell
+
+                    if cell.up:
+                        self.stdscr.addstr(cy, cx, "███", 1)
+                    else:
+                        self.stdscr.addstr(cy, cx, "█  ", 1)
+
+                    if y == self.maze.height - 1:
+                        self.stdscr.addstr(cy + 2, cx, "████", 1)
+                    if cell.right and x == self.maze.width - 1:
+                        self.stdscr.addstr(cy, cx + 3, "█", 1)
+                    if cell.left:
+                        self.stdscr.addstr(cy + 1, cx, "█", 1)
+                        if cell.is_start:
+                            self.stdscr.addstr(cy + 1, cx + 1, "SS", 3)
+                        if cell.is_end:
+                            self.stdscr.addstr(cy + 1, cx + 1, "EE", 2)
+                        if x == width - 1:
+                            self.stdscr.addnstr(cy + 1, cx + 3, "█", 1)
+                    else:
+                        if x == width - 1:
+                            self.stdscr.addstr(cy + 1, cx + 1, "  █", 1)
+                        if x < width - 2 and not cell.right:
+                            self.stdscr.addstr(cy + 1, cx, "    ", 1)
+        except Exception:
+            pass
+
         self.stdscr.refresh()
 
     # Solve prototype
